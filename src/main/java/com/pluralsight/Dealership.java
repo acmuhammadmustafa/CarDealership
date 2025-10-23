b2284911adf003065626b0aa5db04697e3b46995
origin <acmuhammadmustafa@gmail.com>
2025-10-23 16:03:46 -0400

    done for 10/23/2025

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,46 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealership {
+
+    private String name;
+    private String address;
+    private String phone;
+
+// -----------------------------------------------------------------------
+    // Constructors:
+
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+
+    private ArrayList<Vehicle> inventory;
+
+
+// -----------------------------------------------------------------------
+
+    Dealership(String name, String address, String phone){
+        List<Vehicle>
+    }
+
+}
